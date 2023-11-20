b31f569b0cd914c70ac2818cfd19ff5cd79414f2
Mitchell Dederer <mitchelldederer@gmail.com>
2023-11-20 17:57:14 +1100

    feat: adjusted layout

--- src/components/uishell/Layout.tsx
+++ src/components/uishell/Layout.tsx
@@ -1,14 +1,14 @@
 import {Outlet} from "react-router-dom";
 import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+import styles from "./Layout.module.scss";
 
 export default function Layout() {
-    return <>
+    return <div className={styles.container}>
         <Header />
-        <main>
+        <main className={styles.main}>
             <Outlet />
         </main>
-        <footer>
-            <h1>Footer</h1>
-        </footer>
-    </>
+        <Footer />
+    </div>
 }
