8bc1fdbf507ccb10b0118f51a0122300732b73af
Mitchell Dederer <mitchelldederer@gmail.com>
2023-11-20 18:51:33 +1100

    feat: improvements to splash screen

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+//
+// interface TitleProps {
+//     text: string;
+//     type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+// }
+// export default function Title({text, type = "h3"}: TitleProps) {
+//     let className;
+//     switch (type) {
+//         case "h1":
+//             className = ""
+//     }
+//     const element = React.createElement(type, {})
+// }
--- src/components/screens/SplashScreen.tsx
+++ src/components/screens/SplashScreen.tsx
@@ -1,8 +1,18 @@
+import styles from "./SplashScreen.module.scss";
 import {Link} from "react-router-dom";
 
 export default function SplashScreen() {
     return <>
-        <h1>SplashScreen</h1>
-        <Link to="/login">Login</Link>
+        <h1 className={styles.title}>Mailmon</h1>
+        <div className={styles.subtitle}>Accelerate your business with mail made simple.</div>
+
+        <div className={styles.buttonGroup}>
+            <Link to={"/login"} className={styles.button}>
+                Login
+            </Link>
+            <Link to={"/signup"} className={styles.button}>
+                Sign Up
+            </Link>
+        </div>
     </>
 }
